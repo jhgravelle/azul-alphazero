51dfd0807afb3577304ed97b1bee4ccb23e32698
Joe <jhgravelle@gmail.com>
2026-05-16 15:44:03 -0400

    fix: update black version in pre-commit config to match local environment

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -12,8 +12,7 @@ def test_pending_scoring_bug_round1_turn2():
     At turn 2, Player 2 places 2 black tiles on pattern line row 2.
     Expected earned: 1. Bug would give 4 (marking all cells as pending).
     """
-    game = Game.from_string(
-        """
+    game = Game.from_string("""
         R1:T01 [1696940010]                               BAG 15 15 16 18 16
           P1: Player 1   0(  0)  > P2: Player 2   0(  0)  CLR  B  Y  R  K  W
                   . | . . . . .            . | . . . . .  F-1  .  2  1  .  1
@@ -22,8 +21,7 @@ def test_pending_scoring_bug_round1_turn2():
             . . . . | . . . . .      . . . . | . . . . .  F-4  1  1  .  .  2
           . . . . . | . . . . .    . . . . . | . . . . .  F-5  1  .  .  2  1
             ....... |                ....... |            CTR  .  1  1  .  .  F
-        """
-    )
+        """)
 
     # Player 2: 2 black tiles from factory 5 to pattern line row 2
     move = Move.from_str("2K-52")
